fff15ee0865f4de19148a7c90311a9795fd9eb14
muhammadhasssan2006 <hassanweb365@gmail.com>
2025-09-21 15:12:54 +0500

    protfolio

---
+++ app.js
@@ -0,0 +1,8 @@
+// const currentLocation = location.pathname.split("/").pop(); // current file name
+// const navLinks = document.querySelectorAll(".nav-link");
+
+// navLinks.forEach(link => {
+//   if (link.getAttribute("href") === currentLocation) {
+//     link.classList.add("active");
+//   }
+// });
